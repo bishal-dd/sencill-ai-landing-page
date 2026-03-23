e7715488097d5c1dd66903bcd116d857f2380465
Choedra <choedra999@gmail.com>
2026-03-23 11:26:53 +0600

    move site showcase component below features

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
       <Header />
       <Hero />
       <Features />
-      <HowItWorks />
       <SiteShowcase />
+      <HowItWorks />
       <Pricing />
       {/*<Testimonials />*/}
       <CTA />
